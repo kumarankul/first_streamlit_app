f952eb113baf350b9a6e09e132d19cec426fdcde
kumarankul <147072023+kumarankul@users.noreply.github.com>
2023-10-20 14:23:10 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -46,7 +46,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-import snowflake.connector
+#import snowflake.connector
 
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_cur = my_cnx.cursor()
