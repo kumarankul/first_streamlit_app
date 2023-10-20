c674364499aac15b4d5c7708772725026dbee2ed
kumarankul <147072023+kumarankul@users.noreply.github.com>
2023-10-20 14:31:17 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -54,7 +54,6 @@ except URLError as e:
   my_data_row = my_cur.fetchall()
   streamlit.header("The fruit load list contains:")
   streamlit.dataframe(my_data_row)
-
 #streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
